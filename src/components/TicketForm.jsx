a2f46bc597ba839d274bf929aa48420c4d3f7dad
AhlawatAbhishek <abhishekjobsid@pm.me>
2024-07-28 13:51:57 +0530

    refactor: Improve structure and add TicketForm component

--- src/components/TicketForm.jsx
+++ src/components/TicketForm.jsx
@@ -25,7 +25,7 @@ export default function TicketForm() {
   return (
     <form onSubmit={handelSubmit} className="ticket-form">
       <div>
-        <lable>Title</lable>
+        <label>Title</label>
         <input
           type="text"
           value={title}
@@ -33,6 +33,33 @@ export default function TicketForm() {
           onChange={(e) => setTitle(e.target.value)}
         ></input>
       </div>
+      <div>
+        <label>Description</label>
+        <textarea
+          type="text"
+          value={description}
+          className="form-input"
+          onChange={(e) => setDescription(e.target.value)}
+        ></textarea>
+      </div>
+      <fieldset className="priority-fieldset">
+        <legend>Priority</legend>
+        {Object.entries(priorityLabels).map(([value, label]) => (
+          <label key={value} className="priority-label">
+            <input
+              type="radio"
+              value={value}
+              checked={priority === value}
+              className="priority-input"
+              onChange={(e) => setPriority(e.target.value)}
+            ></input>
+            {label}
+          </label>
+        ))}
+      </fieldset>
+      <button type="submit" className="button">
+        submit
+      </button>
     </form>
   );
 }
